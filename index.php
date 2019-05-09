7799ee2dcd3a529b9a2aed8a0cc613ad04bd93e9
wwhitty0 <wwhitty@live.com>
2019-05-09 14:41:59 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,63 @@
+<?php $ip = $_SERVER["REMOTE_ADDR"];?>
+<!DOCTYPE html>
+<html lang="en" >
+<style>
+body {
+  background: url(http://vitty.host/images/BG-HEX.png) no-repeat center fixed;
+    -webkit-background-size: cover;
+	-moz-background-size: cover;
+	-o-background-size: cover;
+ 	background-size: cover;
+ 	background-size: 100% 100%;
+ 	font-family: Orbitron, sans-serif;
+}
+.custom{
+  width:70%;
+  margin:0 auto;
+  min-width:350px;
+  font-family: Orbitron, sans-serif;
+}
+</style>
+<head>
+  <meta charset="UTF-8">
+  <title>FUCK OFF</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.1/css/bootstrap.css'>
+
+<link rel="stylesheet" href="css/remote.css">
+
+</head>
+
+<body>
+<center>
+  <div class="container-fluid my-3"></br></br></br>
+  <div class="row">
+    <div class="col-md-12">
+      <div class="card border-success mb-3 custom">
+        <div class="card-header">
+<h1 style="text-align: center;">YOUR LOST!!!!</h1>
+<p>&nbsp;</p>
+<p style="text-align: center;">&nbsp;<img src="https://i.pinimg.com/originals/08/9d/b5/089db5d19a2ac565136859033c2d5575.png" alt="" width="250" height="200" /></p>
+<h1 style="text-align: center;"><span style="color: #ff0000;"><em>NOW FUCK OFF!!!</em></span></h1>
+<h4 style="text-align: center;"><em>BUT BE SURE TO HAVE A NICE DAY!</em></h4>
+<p>&nbsp;</p>
+
+    </div>
+  </div><!-- /.row -->
+
+
+</div>
+
+</center>
+<form class="form-3"><center>
+				    <p class="clearfix">
+				        <label for="login">YOUR IP IS LOGGED AS: <?php echo " $ip"; ?></label>
+				    </p>
+				</center></form>
+  <p style="text-align: center;"><img src="https://upload.wikimedia.org/wikipedia/hif/b/bd/Liverpool_FC.png" alt="" width="200" height="250" /></p>
+
+</body>
+
+</html>
+
